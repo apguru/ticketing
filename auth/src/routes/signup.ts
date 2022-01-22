a8f3ce0d48ceb1ff38c9c2be40d8bcc330403b78
Alex <spielexd@hotmail.de>
2022-01-22 11:24:36 +0100

    added user creation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import RequestValidationError from "../errors/request-validation-error";
-// import DatabaseConnectionError from "../errors/database-connection-error";
+import User from "../models/user";
 
 const router = express.Router();
 
@@ -14,17 +14,22 @@ router.post(
       .isLength({ min: 4 })
       .withMessage("Password must be at least 4 characters long"),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
-    // const { email, password } = req.body;
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
 
-    console.log("creating a user...");
-    // throw new DatabaseConnectionError();
+    if (existingUser) {
+      return res.send({});
+    }
 
-    res.send("User created");
+    const user = User.build({ email, password });
+    await user.save();
+
+    return res.status(201).send(user);
   }
 );
 
